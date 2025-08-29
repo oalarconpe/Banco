5a27441542fccb3ac928c2f6addbcf69ec9890d2
orlapez <orlapez@gmail.com>
2025-08-29 08:00:32 -0500

    Banco

---
+++ Banco/CuentaBancaria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class CuentaBancaria
+    {
+
+
+        //Campos
+        private string Nombre, Apellido, Direccion, Cedula;
+        private double Saldo;
+
+
+        //Constructor
+        public CuentaBancaria(string NombrePa, string ApellidoPa, string DireccionPa, string CedulaPa, double SaldoPa)
+        {
+            Nombre = NombrePa;
+            Apellido = ApellidoPa;
+            Direccion = DireccionPa;
+            Cedula = CedulaPa;
+            Saldo = SaldoPa;
+        }
+
+        //Metodos
+        public void Depositar(double MontoPa)
+        {
+            Saldo += MontoPa;
+        }
+
+        public void Retirar(double MontoPa)
+        {
+            if (MontoPa > Saldo)
+            {
+                Console.WriteLine("Fondos insuficientes");
+            }
+            else
+            {
+                Saldo -= MontoPa;
+            }
+        }
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine("El saldo actual es: " + Saldo);
+        }
+
+        public override string ToString()
+        {
+            return "Nombre: " + Nombre + "\nApellido: " + Apellido + "\nDireccion: " + Direccion + "\nCedula: " + Cedula + "\nSaldo: " + Saldo;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //variables
+
+            double MontoAr, SaldoInicialAr;
+            int opcion;
+            string NombreAr, ApellidoAr, DireccionAr, CedulaAr;
+
+            //Creacion de la cuenta bancaria
+            Console.Write("Estás a punto de crear una nueva cuenta bancaria, presiona cualquier tecla: ");
+            Console.ReadKey();
+            Console.Write("\nIngrese su nombre: ");
+            NombreAr = Console.ReadLine();
+            Console.Write("Ingrese su apellido: ");
+            ApellidoAr = Console.ReadLine();
+            Console.Write("Ingrese su direccion: ");
+            DireccionAr = Console.ReadLine();
+            Console.Write("Ingrese su cedula: ");
+            CedulaAr = Console.ReadLine();
+            Console.Write("Ingrese el saldo inicial: ");
+            SaldoInicialAr = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el valor del primer deposito: $ ");
+            MontoAr = Convert.ToDouble(Console.ReadLine());
+
+            CuentaBancaria cliente = new CuentaBancaria(NombreAr, ApellidoAr, DireccionAr, CedulaAr, SaldoInicialAr);
+
+
+            Console.Write("haz creado una nueva cuenta bancaria, felicitaciones, presiona cualquier tecla: ");
+            Console.ReadKey();
+
+
+            do {                 
+                Console.WriteLine("Bienvenido al banco, " + NombreAr + " " + ApellidoAr);
+                Console.WriteLine("\n1. Depositar");
+                Console.WriteLine("2. Retirar");
+                Console.WriteLine("3. Consultar saldo");
+                Console.WriteLine("4. Mostrar datos de la cuenta");
+                Console.WriteLine("5. Salir");
+                Console.Write("Ingrese una opcion: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch(opcion)
+                {
+                    case 1:
+                        {
+                            Console.Write("Ingrese el monto a depositar: $ ");
+                            MontoAr = Convert.ToDouble(Console.ReadLine());
+                            cliente.Depositar(MontoAr);
+                            Console.Write("Deposito exitoso, presiona cualquier tecla para continuar: ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Ingrese el monto a retirar: $ ");
+                            MontoAr = Convert.ToDouble(Console.ReadLine());
+                            cliente.Retirar(MontoAr);
+                            Console.Write("Retiro exitoso, presiona cualquier tecla para continuar: ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case 3:
+                        {
+                            cliente.ConsultarSaldo();
+                            Console.Write("Presiona cualquier tecla para continuar: ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine(cliente.ToString());
+                            Console.Write("Presiona cualquier tecla para continuar: ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.Write("Gracias por usar nuestro servicio, presiona cualquier tecla para salir: ");
+                            Console.ReadKey();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Write("Opcion no valida, presiona cualquier tecla para continuar: ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+        }
+
+
+
+
+
+
+
+            } while (opcion >= 1 && opcion <= 4);
+
+
+
+
+
+        }
+    }
+}
